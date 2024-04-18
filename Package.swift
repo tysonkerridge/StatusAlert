66cde691c00536a87892eb1f397a1999111491dc
Tyson Kerridge <tysonkerridge@gmail.com>
2024-04-18 17:12:09 +1000

    Update Package.swift
    remove version and update tools

--- Package.swift
+++ Package.swift
@@ -1,13 +1,10 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "StatusAlert",
-    platforms: [
-       .iOS(.v9),
-    ],
     products: [
         .library(name: "StatusAlert", targets: ["StatusAlert"]),
     ],
